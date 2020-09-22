3e59b9c4f87b7458be34c0471482cefd72833d2c
Francisco Ciol R. Aveiro <francisco@pop-os.localdomain>
2020-09-22 18:52:41 -0300

    entrega 1

--- renderizador.py
+++ renderizador.py
@@ -9,18 +9,136 @@ import gpu          # Simula os recursos de uma GPU
 
 def polypoint2D(point, color):
     """ Função usada para renderizar Polypoint2D. """
-    gpu.GPU.set_pixel(3, 1, 255, 0, 0) # altera um pixel da imagem
-    # cuidado com as cores, o X3D especifica de (0,1) e o Framebuffer de (0,255)
+    # Converte o esquema de cores
+    color = [int(i * 255) for i in color]
+    for i in range(int(len(point)//2)):
+        # Transforma de Float para inteiro cada X e Y
+        y = int(point[(i*2)])
+        x = int(point[(2*i)+1])
+
+        # Define onde desenhar o pixel e com que cor
+        gpu.GPU.set_pixel(
+            y, x, color[0], color[1], color[2])
+
 
 def polyline2D(lineSegments, color):
     """ Função usada para renderizar Polyline2D. """
-    x = gpu.GPU.width//2
-    y = gpu.GPU.height//2
-    gpu.GPU.set_pixel(x, y, 255, 0, 0) # altera um pixel da imagem
+    y1 = (lineSegments[0])
+    y2 = (lineSegments[2])
+    x1 = (lineSegments[1])
+    x2 = (lineSegments[3])
+    dx = abs(x2-x1)
+    dy = abs(y2-y1)
+
+    # Decide se usa X ou Y como base do FOR
+    if(dx > dy):
+
+        if(x1 < x2):
+            # Comeca no ponto 1 e vai para ponto 2 - FOR em X
+            s = (y2-y1)/(x2-x1)
+            v = y1
+            for u in range(int(x1), int(x2)+1):
+                # altera um pixel da imagem
+                gpu.GPU.set_pixel(int((v)//1), u, 255 *
+                                  color[0], 255*color[1], 255*color[2])
+                v += s
+        else:
+            # Comeca no ponto 2 e vai para ponto 1 - FOR em X
+            s = (y1-y2)/(x1-x2)
+            v = y2
+            for u in range(int(x2), int(x1)+1):
+                # altera um pixel da imagem
+                gpu.GPU.set_pixel(int((v)//1), u, 255 *
+                                  color[0], 255*color[1], 255*color[2])
+                v += s
+
+    else:
+
+        if(y1 < y2):
+            # Comeca no ponto 1 e vai para ponto 2 - FOR em Y
+            s = (x2-x1)/(y2-y1)
+            u = x1
+            for v in range(int(y1), int(y2)+1):
+                # altera um pixel da imagem
+                gpu.GPU.set_pixel(v, int((u)//1), 255 *
+                                  color[0], 255*color[1], 255*color[2])
+                u += s
+        else:
+            # Comeca no ponto 2 e vai para ponto 1 - FOR em Y
+            s = (x1-x2)/(y1-y2)
+            u = x2
+            for v in range(int(y2), int(y1)+1):
+                # altera um pixel da imagem
+                gpu.GPU.set_pixel(v, int((u)//1), 255 *
+                                  color[0], 255*color[1], 255*color[2])
+                u += s
+
+        # gpu.GPU.set_pixel( int((v)//1), u, 255*color[0], 255*color[1], 255*color[2]) # altera um pixel da imagem
+
 
 def triangleSet2D(vertices, color):
     """ Função usada para renderizar TriangleSet2D. """
-    gpu.GPU.set_pixel(24, 8, 255, 255, 0) # altera um pixel da imagem
+    # Taxa de Supersampling
+    supersample = 10
+
+    # Salva os valores originais dos vertices
+    y0_orig = vertices[0]
+    x0_orig = vertices[1]
+    y1_orig = vertices[2]
+    x1_orig = vertices[3]
+    y2_orig = vertices[4]
+    x2_orig = vertices[5]
+
+    # Valores dos vertices com supersampling
+    y0 = y0_orig * supersample
+    x0 = x0_orig * supersample
+    y1 = y1_orig * supersample
+    x1 = x1_orig * supersample
+    y2 = y2_orig * supersample
+    x2 = x2_orig * supersample
+
+    x_min = round(min(x0, x1, x2))
+    x_max = round(max(x0, x1, x2))
+    y_min = round(min(y0, y1, y2))
+    y_max = round(max(y0, y1, y2))
+
+    # lista que vai guardar as coordenadas dos pixeis a serem desenhados, com supersampling
+    lista_miniPixels = []
+
+    # Verifica para cada pixel dentro do quadrado que contem o triangulo, se deve desenha-lo
+    # calc0,calc1,calc2 
+    for v in range(y_min-1, y_max+1):
+        for u in range(x_min-1, x_max+1):
+            
+            calc1 = (y1-y2)*(u+0.5) + (x2-x1)*(v+0.5) + (x1*y2 - x2*y1)
+            if((y1-y2)*(x0) + (x2-x1)*(y0) + (x1*y2 - x2*y1) < 0):
+                calc1 *= -1
+
+            if(calc1 >= 0):
+                calc2 = (y0-y2)*(u+0.5) + (x2-x0)*(v+0.5) + (x0*y2 - x2*y0)
+                if((y0-y2)*(x1) + (x2-x0)*(y1) + (x0*y2 - x2*y0) < 0):
+                    calc2*=-1
+                if(calc2 > 0):
+
+                    calc3 = (y1-y0)*(u+0.5) + (x0-x1)*(v+0.5) + (x1*y0 - x0*y1)
+                    if((y1-y0)*(x2) + (x0-x1)*(y2) + (x1*y0 - x0*y1) < 0):
+                        calc3 *= -1
+                    if(calc3>=0):
+                        lista_miniPixels.append([u,v])
+    
+    x_min = round(min(x0_orig, x1_orig, x2_orig))
+    x_max = round(max(x0_orig, x1_orig, x2_orig))
+    y_min = round(min(y0_orig, y1_orig, y2_orig))
+    y_max = round(max(y0_orig, y1_orig, y2_orig))
+
+    for v in range(y_min-1, y_max+1):
+        for u in range(x_min-1, x_max+1):
+            intensity = sum(1 for i in lista_miniPixels if (i[0]//supersample == u and i[1]//supersample == v))/(supersample**2)
+            # altera um pixel da imagem
+            
+            if(intensity>0):
+                gpu.GPU.set_pixel(
+                    v, u, 255*color[0]*intensity, 255*color[1]*intensity, 255*color[2]*intensity)
 
 def triangleSet(point, color):
     """ Função usada para renderizar TriangleSet. """
