85fe4594b88c9cc800f600b780c4f3a91ba33c30
Francisco Ciol R. Aveiro <fran.aveiro@hotmail.com>
2020-10-03 21:39:43 -0300

    Entrega parcial projeto 3 - Falta acertar o color do exemplo 8

--- renderizador.py
+++ renderizador.py
@@ -13,8 +13,11 @@ import numpy as np
 PILHA_TRANSFORM = [np.identity(4)]
 
 # Dimensoes tela
-LARGURA = 900
-ALTURA = 900
+LARGURA = 1200
+ALTURA = 720
+
+# Taxa de Supersampling
+supersample = 1
 
 
 def polypoint2D(point, color):
@@ -88,8 +91,6 @@ def polyline2D(lineSegments, color):
 
 def triangleSet2D(vertices, color):
     """ Função usada para renderizar TriangleSet2D. """
-    # Taxa de Supersampling
-    supersample = 2
 
     # Salva os valores originais dos vertices
     y0_orig = vertices[0]
@@ -151,8 +152,169 @@ def triangleSet2D(vertices, color):
                 gpu.GPU.set_pixel(
                     v, u, 255*color[0]*intensity, 255*color[1]*intensity, 255*color[2]*intensity)
 
+def triangleSet2DTexture(vertices, coords, indx, matrix):
+    """ Função usada para renderizar TriangleSet2D. """
+    matrix = gpu.GPU.load_texture(matrix[0])
+    # Salva os valores originais dos vertices
 
-def triangleSet(point, color):
+    y0_orig = vertices[0]
+    x0_orig = vertices[1]
+    y1_orig = vertices[2]
+    x1_orig = vertices[3]
+    y2_orig = vertices[4]
+    x2_orig = vertices[5]
+
+    # Valores dos vertices com supersampling
+    y0 = y0_orig * supersample
+    x0 = x0_orig * supersample
+    y1 = y1_orig * supersample
+    x1 = x1_orig * supersample
+    y2 = y2_orig * supersample
+    x2 = x2_orig * supersample
+
+    x_min = round(min(x0, x1, x2))
+    x_max = round(max(x0, x1, x2))
+    y_min = round(min(y0, y1, y2))
+    y_max = round(max(y0, y1, y2))
+
+    # lista que vai guardar as coordenadas dos pixeis a serem desenhados, com supersampling
+    lista_miniPixels = []
+
+    # Verifica para cada pixel dentro do quadrado que contem o triangulo, se deve desenha-lo
+    # calc0,calc1,calc2
+    y_l = range(y_min-1, y_max+1)
+    x_l = range(x_min-1, x_max+1)
+    for idx_v, v in enumerate(y_l):
+        for idx_u, u in enumerate(x_l):
+
+
+            calc1 = (y1-y2)*(u+0.5) + (x2-x1)*(v+0.5) + (x1*y2 - x2*y1)
+            if((y1-y2)*(x0) + (x2-x1)*(y0) + (x1*y2 - x2*y1) < 0):
+                calc1 *= -1
+
+            if(calc1 >= 0):
+                calc2 = (y0-y2)*(u+0.5) + (x2-x0)*(v+0.5) + (x0*y2 - x2*y0)
+                if((y0-y2)*(x1) + (x2-x0)*(y1) + (x0*y2 - x2*y0) < 0):
+                    calc2 *= -1
+                if(calc2 > 0):
+
+                    calc3 = (y1-y0)*(u+0.5) + (x0-x1)*(v+0.5) + (x1*y0 - x0*y1)
+                    if((y1-y0)*(x2) + (x0-x1)*(y2) + (x1*y0 - x0*y1) < 0):
+                        calc3 *= -1
+                    if(calc3 >= 0):
+                        
+                        lista_miniPixels.append([u, v])
+                        
+
+    x_min = round(min(x0_orig, x1_orig, x2_orig))
+    x_max = round(max(x0_orig, x1_orig, x2_orig))
+    y_min = round(min(y0_orig, y1_orig, y2_orig))
+    y_max = round(max(y0_orig, y1_orig, y2_orig))
+
+    y_l = range(y_min-1, y_max+1)
+    x_l = range(x_min-1, x_max+1)
+    for idx_v, v in enumerate(y_l):
+        for idx_u, u in enumerate(x_l):
+
+
+            intensity = sum(1 for i in lista_miniPixels if (
+                i[0]//supersample == u and i[1]//supersample == v))/(supersample**2)
+            # altera um pixel da imagem
+
+            if(intensity > 0):
+                idx_u1 = int(idx_u/len(x_l)*matrix.shape[1])
+                idx_v1 = int(idx_v/len(y_l)*matrix.shape[0])
+
+
+                color = matrix[idx_u1,idx_v1]
+
+
+                gpu.GPU.set_pixel(
+                    v, u, color[0]*intensity, color[1]*intensity, color[2]*intensity)
+
+def triangleSet2DColor(vertices, colors):
+    """ Função usada para renderizar TriangleSet2D. """
+
+    # Salva os valores originais dos vertices
+    y0_orig = vertices[0]
+    x0_orig = vertices[1]
+    y1_orig = vertices[2]
+    x1_orig = vertices[3]
+    y2_orig = vertices[4]
+    x2_orig = vertices[5]
+
+    c1 = colors[0:3]
+    c2 = colors[3:6]
+    c3 = colors[6:9]
+
+    # Valores dos vertices com supersampling
+    y0 = y0_orig * supersample
+    x0 = x0_orig * supersample
+    y1 = y1_orig * supersample
+    x1 = x1_orig * supersample
+    y2 = y2_orig * supersample
+    x2 = x2_orig * supersample
+
+    x_min = round(min(x0, x1, x2))
+    x_max = round(max(x0, x1, x2))
+    y_min = round(min(y0, y1, y2))
+    y_max = round(max(y0, y1, y2))
+
+    # lista que vai guardar as coordenadas dos pixeis a serem desenhados, com supersampling
+    lista_miniPixels = []
+
+    # Verifica para cada pixel dentro do quadrado que contem o triangulo, se deve desenha-lo
+    # calc0,calc1,calc2
+    for v in range(y_min-1, y_max+1):
+        for u in range(x_min-1, x_max+1):
+
+            calc1 = (y1-y2)*(u+0.5) + (x2-x1)*(v+0.5) + (x1*y2 - x2*y1)
+            if((y1-y2)*(x0) + (x2-x1)*(y0) + (x1*y2 - x2*y1) < 0):
+                calc1 *= -1
+
+            if(calc1 >= 0):
+                calc2 = (y0-y2)*(u+0.5) + (x2-x0)*(v+0.5) + (x0*y2 - x2*y0)
+                if((y0-y2)*(x1) + (x2-x0)*(y1) + (x0*y2 - x2*y0) < 0):
+                    calc2 *= -1
+                if(calc2 > 0):
+
+                    calc3 = (y1-y0)*(u+0.5) + (x0-x1)*(v+0.5) + (x1*y0 - x0*y1)
+                    if((y1-y0)*(x2) + (x0-x1)*(y2) + (x1*y0 - x0*y1) < 0):
+                        calc3 *= -1
+                    if(calc3 >= 0):
+                        lista_miniPixels.append([u, v])
+
+    x_min = round(min(x0_orig, x1_orig, x2_orig))
+    x_max = round(max(x0_orig, x1_orig, x2_orig))
+    y_min = round(min(y0_orig, y1_orig, y2_orig))
+    y_max = round(max(y0_orig, y1_orig, y2_orig))
+
+    for v in range(y_min-1, y_max+1):
+        for u in range(x_min-1, x_max+1):
+            intensity = sum(1 for i in lista_miniPixels if (
+                i[0]//supersample == u and i[1]//supersample == v))/(supersample**2)
+
+            # altera um pixel da imagem
+            alpha = -(u-x1)*(y2-y1)+(v-y1)*(x2-x1) / \
+                (-(x0-x1)*(y2-y1)+(y0-y1)*(x2-x1))
+
+            beta = -(u-x2)*(y0-y2)+(v-y2)*(x0-x2) / \
+                (-(x1-x2)*(y0-y2)+(y1-y2)*(x0-x2))
+
+            gama = 1 - alpha - beta
+            
+            color_pix = np.add(np.multiply(c1,alpha),np.multiply(c2,beta))
+            
+            color_pix = np.add(color_pix,np.multiply(c3,gama))
+            
+            color_pix = np.multiply(color_pix,(255*intensity))
+            
+            if(intensity > 0):
+                gpu.GPU.set_pixel(
+                    v, u, color_pix[0], color_pix[1], color_pix[2])
+
+
+def triangleSet(point, color, colors=False, Textures=False, current_texture=None, text_coords=None, text_color_index=None):
     """ Função usada para renderizar TriangleSet. """
     # Nessa função você receberá pontos no parâmetro point, esses pontos são uma lista
     # de pontos x, y, e z sempre na ordem. Assim point[0] é o valor da coordenada x do
@@ -189,8 +351,12 @@ def triangleSet(point, color):
         points.extend(conf_tela[2][:2])
 
         # Envio para a função de Desenho
-        triangleSet2D(points, color)
-
+        if(Textures):
+            triangleSet2DTexture(points, text_coords, text_color_index, current_texture)
+        elif(colors):
+            triangleSet2DColor(points, color)
+        else:
+            triangleSet2D(points,color)
 
 def viewpoint(position, orientation, fieldOfView):
     """ Função usada para renderizar (na verdade coletar os dados) de Viewpoint. """
@@ -282,7 +448,7 @@ def triangleStripSet(point, stripCount, color):
     # por diante. No TriangleStripSet a quantidade de vértices a serem usados é informado
     # em uma lista chamada stripCount (perceba que é uma lista).
 
-    # Cria a lista de pontos e adiciona todos os pontos da lista 
+    # Cria a lista de pontos e adiciona todos os pontos da lista
     triangs = []
     for i in range(int(stripCount[0])-2):
         cur = i
@@ -308,7 +474,7 @@ def indexedTriangleStripSet(point, index, color):
     # primeiro triângulo será com os vértices 0, 1 e 2, depois serão os vértices 1, 2 e 3,
     # depois 2, 3 e 4, e assim por diante.
 
-    # Cria a lista de pontos e adiciona todos os pontos da lista 
+    # Cria a lista de pontos e adiciona todos os pontos da lista
     triangs = []
     for i in range(len(index)-3):
         cur = int(index[int(i)])
@@ -329,76 +495,58 @@ def box(size, color):
     # e Z, respectivamente, e cada valor do tamanho deve ser maior que zero. Para desenha
     # essa caixa você vai provavelmente querer tesselar ela em triângulos, para isso
     # encontre os vértices e defina os triângulos.
-
     # coordenadas positivas da caixa
     x = size[0]/2
     y = size[1]/2
     z = size[2]/2
-
     # lista de pontos
     triangs = []
-
-   
-
     # z negativo fixo
     triangs.extend([-x, y, -z])
     triangs.extend([-x, -y, -z])
     triangs.extend([x, y, -z])
-
     triangs.extend([x, -y, -z])
     triangs.extend([-x, -y, -z])
     triangs.extend([x, y, -z])
-
     # y positivo fixo
     triangs.extend([-x, y, z])
     triangs.extend([-x, y, -z])
     triangs.extend([x, y, z])
-
     triangs.extend([x, y, z])
     triangs.extend([-x, y, -z])
     triangs.extend([x, y, -z])
-
     # y negativo fixo
     triangs.extend([-x, -y, z])
     triangs.extend([-x, -y, -z])
     triangs.extend([x, -y, z])
-
     triangs.extend([x, -y, z])
     triangs.extend([-x, -y, -z])
     triangs.extend([x, -y, -z])
-
     # x positivo fixo
     triangs.extend([x, y, z])
     triangs.extend([x, -y, -z])
     triangs.extend([x, y, -z])
-
     triangs.extend([x, y, z])
     triangs.extend([x, -y, -z])
     triangs.extend([x, -y, z])
-
     # x negativo fixo
     triangs.extend([-x, y, z])
     triangs.extend([-x, -y, -z])
     triangs.extend([-x, y, -z])
-
     triangs.extend([-x, y, z])
     triangs.extend([-x, -y, -z])
     triangs.extend([-x, -y, z])
-
-     # z positivo fixo
+    # z positivo fixo
     triangs.extend([-x, y, z])
     triangs.extend([-x, -y, z])
     triangs.extend([x, y, z])
-
     triangs.extend([x, -y, z])
     triangs.extend([-x, -y, z])
     triangs.extend([x, y, z])
-
     # manda os pontos para desenhar
     triangleSet(triangs, color)
-    
 
-    
+
 def indexedFaceSet(coord, coordIndex, colorPerVertex, color, colorIndex, texCoord, texCoordIndex, current_color, current_texture):
     """ Função usada para renderizar IndexedFaceSet. """
     # A função indexedFaceSet é usada para desenhar malhas de triângulos. Ela funciona de
@@ -412,13 +560,55 @@ def indexedFaceSet(coord, coordIndex, colorPerVertex, color, colorIndex, texCoor
     # acabou. A ordem de conexão será de 3 em 3 pulando um índice. Por exemplo: o
     # primeiro triângulo será com os vértices 0, 1 e 2, depois serão os vértices 1, 2 e 3,
     # depois 2, 3 e 4, e assim por diante.
-    # Adicionalmente essa implementação do IndexedFace suport cores por vértices, assim
+    # Adicionalmente essa implementação do IndexedFace suporta cores por vértices, assim
     # a se a flag colorPerVertex estiver habilidades, os vértices também possuirão cores
     # que servem para definir a cor interna dos poligonos, para isso faça um cálculo
     # baricêntrico de que cor deverá ter aquela posição. Da mesma forma se pode definir uma
     # textura para o poligono, para isso, use as coordenadas de textura e depois aplique a
     # cor da textura conforme a posição do mapeamento. Dentro da classe GPU já está
-    # implementadado um método para a leitura de imagens.    
+    # implementadado um método para a leitura de imagens.
+    if (colorPerVertex):
+        for i in range(0, len(coordIndex)-2):
+            if(not ((-1) == coordIndex[i] or (-1) == coordIndex[i+1] or coordIndex[i+2] == (-1))):
+                p1 = coord[coordIndex[i]*3:(coordIndex[i]+1)*3]
+                p2 = coord[coordIndex[i+1]*3:(coordIndex[i+1]+1)*3]
+                p3 = coord[coordIndex[i+2]*3:(coordIndex[i+2]+1)*3]
+
+                c1 = color[colorIndex[i]*3:(colorIndex[i]+1)*3]
+                c2 = color[colorIndex[i+1]*3:(colorIndex[i+1]+1)*3]
+                c3 = color[colorIndex[i+2]*3:(colorIndex[i+2]+1)*3]
+
+                c1.extend(c2)
+                c1.extend(c2)
+                p1.extend(p2)
+                p1.extend(p3)
+                triangleSet(p1, c1, True)
+                
+    elif(texCoord):
+        points_txt = 0
+        for i in range(0, len(coordIndex)-2):
+            if(not ((-1) == coordIndex[i] or (-1) == coordIndex[i+1] or coordIndex[i+2] == (-1))):
+                p1 = coord[coordIndex[i]*3:(coordIndex[i]+1)*3]
+                p2 = coord[coordIndex[i+1]*3:(coordIndex[i+1]+1)*3]
+                p3 = coord[coordIndex[i+2]*3:(coordIndex[i+2]+1)*3]
+                p1.extend(p2)
+                p1.extend(p3)
+                txt = []
+                txt.extend([texCoordIndex[points_txt*3+points_txt],texCoordIndex[points_txt*3+1+points_txt],texCoordIndex[points_txt*3+2+points_txt]])
+                points_txt+=1
+                triangleSet(p1, None, False, True, current_texture, texCoord,txt)
+                
+        
+        
+    else:
+        for i in range(0, len(coordIndex)-2):
+            if(not ((-1) == coordIndex[i] or (-1) == coordIndex[i+1] or coordIndex[i+2] == (-1))):
+                p1 = coord[coordIndex[i]*3:(coordIndex[i]+1)*3]
+                p2 = coord[coordIndex[i+1]*3:(coordIndex[i+1]+1)*3]
+                p3 = coord[coordIndex[i+2]*3:(coordIndex[i+2]+1)*3]
+                p1.extend(p2)
+                p1.extend(p3)
+                triangleSet(p1, current_color, False)
 
 
 if __name__ == '__main__':
